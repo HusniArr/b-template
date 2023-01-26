30d6b2a796425c9c989f643abcdc1a272a307844
HusniArr <husniarrasyid@gmail.com>
2023-01-26 17:06:01 +0700

    tambah file javascript

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-console.log('hello world...')
+function show(){
+    // console.log('hello show...')
+    if(window.pageYOffset >= 1604){
+        console.log('muncul...');
+    } else if(window.pageYOffset <= 1604){
+        console.log('hidden...')
+    }
+}
+window.addEventListener('scroll',show);
+
+let backTotop = document.getElementById('back-to-top');
+backTotop.addEventListener('click',function(){
+    console.log('hello world...')
+})
