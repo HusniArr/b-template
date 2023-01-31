6785e2db92692624d3d1391741e0b5f2f50b0ec3
HusniArr <husniarrasyid@gmail.com>
2023-01-31 10:38:15 +0700

    styling menu-toggle

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 let backTotop = document.getElementById('back-to-top');
 function toggleMenu(){
     let menu = document.getElementById('menu');
-    if(menu.style.display == 'block'){
+    if(menu.style.display === 'block'){
         menu.style.display = 'none';
     }else{
         menu.style.display = 'block';
